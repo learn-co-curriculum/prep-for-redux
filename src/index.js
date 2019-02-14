17e4620ae076705101a26e7d052746781a44f0fc
satyasinha94 <satyasinha94@gmail.com>
2019-02-14 10:13:47 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -5,28 +5,6 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Counter />
-      </div>
-    );
-  }
-}
-
-class Header extends Component {
-  render() {
-    return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-    );
-  }
-}
-
-class Counter extends Component {
   state = { count: 0 };
 
   increment = () => {
@@ -42,14 +20,35 @@ class Counter extends Component {
     const upToNext = 5 - remainder;
     return `The current count is less than ${this.state.count + upToNext}`;
   };
+  render() {
+    return (
+      <div className="App">
+        <Header description={this.renderDescription}/>
+        <Counter increment={this.increment} decrement={this.decrement} count={this.state.count}/>
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>{this.props.description()}</h3>
+      </header>
+    );
+  }
+}
+
+class Counter extends Component {
 
   render() {
     return (
       <div className="Counter">
-        <h1>{this.state.count}</h1>
-        <button onClick={this.decrement}> - </button>
-        <button onClick={this.increment}> + </button>
-        <h3>{this.renderDescription()}</h3>
+        <h1>{this.props.count}</h1>
+        <button onClick={this.props.decrement}> - </button>
+        <button onClick={this.props.increment}> + </button>
       </div>
     );
   }
