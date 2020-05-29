19cb25b97e7bf45f664f13425a8e99cf50f6b2ff
Ann Duong <39737217+thuyanduong-flatiron@users.noreply.github.com>
2020-05-29 09:10:37 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,11 +30,11 @@ class Counter extends Component {
   state = { count: 0 };
 
   increment = () => {
-    this.setState(prevState => ({ count: prevState.count + 1 }));
+    this.setState({ count: this.state.count + 1 });
   };
 
   decrement = () => {
-    this.setState(prevState => ({ count: prevState.count - 1 }));
+    this.setState({ count: this.state.count - 1 });
   };
 
   renderDescription = () => {
